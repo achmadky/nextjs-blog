a8a02614149d4288429edec230e10bf7985fb277
achmadky <achmad.dhikrillah@gmail.com>
2020-09-14 02:59:03 +0700

    Typescript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,34 +3,34 @@ import { getAllPostIds, getPostData } from '../../posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
 
-// require('../')
-
-// import("..")
-
-// import default
-// import Layout from '..'
-
-// named import
-// import {something} from '..'
-
-export default function Post({ postData }) {
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
-    <Layout><Head>
-    <title>{postData.title}</title>
-  </Head>
-  <article>
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
-  </Layout>
+    </Layout>
   )
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds()
   return {
     paths,
@@ -38,8 +38,8 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string)
   return {
     props: {
       postData
--- posts.ts
+++ posts.ts
@@ -23,7 +23,7 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data
+      ...(matterResult.data as { date: string; title: string })
     }
   })
   // Sort posts by date
@@ -36,8 +36,6 @@ export function getSortedPostsData() {
   })
 }
 
-// export const getAllPostIds
-
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory)
   return fileNames.map(fileName => {
@@ -49,7 +47,7 @@ export function getAllPostIds() {
   })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -66,6 +64,6 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data
+    ...(matterResult.data as { date: string; title: string })
   }
 }
